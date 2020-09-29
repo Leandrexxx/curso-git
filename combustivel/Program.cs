9f32d0748f7ac87fb7d17756b8767635a9e4131f
Leandro Rangel <leandrexpsn@gmail.com>
2020-09-29 20:23:43 -0300

    Projeto iniciado do posto de combustivel

---
+++ combustivel/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace combustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, alcool, gasolina, diesel;
+
+            Console.WriteLine("POSTO DE GASOLINA DA QUEBRADA");
+            Console.WriteLine();
+
+            Console.Write("Informe um codigo (1, 2, 3) ou 4 para parar: ");
+            n = int.Parse(Console.ReadLine());
+
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+
+            while ( n != 4)
+            {     
+                if ( n == 1)
+                {
+                    alcool++;
+                } else if ( n == 2)
+                {
+                    gasolina++;
+                } else if ( n == 3)
+                {
+                    diesel++;
+                } else
+                {
+                Console.WriteLine("Codigo fora da faixa de 1 a 4. Por favor tente novamente!");
+                }
+
+                Console.Write("Informe um codigo (1, 2, 3) ou 4 para parar: ");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine();
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+
+
+        }
+    }
+}
